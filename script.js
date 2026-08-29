9af81a82b4984502dd6c0cb0899e67b0adaa8f83
santosoliveiragustavo0703-cmd <santos.oliveira.gustavo0703@escola.pr.gov.br>
2026-08-28 21:15:09 -0300

    mvm

---
+++ script.js
@@ -0,0 +1,10 @@
+const botoes = document.querySelectorA11("button")
+
+botoes.forEach( function(botao) {
+botao.addEventListener("click", botaoClicado);
+
+
+function botaoClicado () (
+    console.log("fui clicado");
+    let texto = botaoquerySelector("span");
+    texto.textContent++
